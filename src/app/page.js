2a0dd172119d2abbc06484bc4b0c84b2d7a04436
dmostafiz <dev.mostafiz@gmail.com>
2024-02-10 23:28:38 +0600

    sdf

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+'use client'
+// import Image from "next/image";
+import { Link } from '@chakra-ui/next-js'
+import { Box, Flex, Image } from '@chakra-ui/react';
+export default function Home() {
+  return (
+    <Box h={'100vh'} w={'full'}>
+      <Flex h={'full'} justify={'center'} alignItems={'center'} >
+        <Image h={'120px'} src='/logo.png' />
+      </Flex>
+    </Box>
+  );
+}
